9cf8abf0e7897a5483fb4f5dc80552a3b98e4528
Léo POULIN <leoraphael.poulin@epitech.eu>
2018-04-13 12:27:03 -0700

    es init

--- gym_referencer/gym_referencer.py
+++ gym_referencer/gym_referencer.py
@@ -1,7 +1,11 @@
 import json
 
+from elasticsearch import Elasticsearch
+
 def main() -> None:
-    pass
+    es_cluster = Elasticsearch([{'host': '127.0.0.1', 'port': 9200}])
+    print(es_cluster.status())
+    print(us_cluster.health())
 
 if __name__ == "__main__":
     main()
