237c65f0a4d91375054e0051bbcf09eaad91d99e
Léo POULIN <leoraphael.poulin@epitech.eu>
2018-04-13 13:01:42 -0700

    wrong methods

--- gym_referencer/gym_referencer.py
+++ gym_referencer/gym_referencer.py
@@ -4,8 +4,7 @@ from elasticsearch import Elasticsearch
 
 def main() -> None:
     es_cluster = Elasticsearch([{'host': '127.0.0.1', 'port': 9200}])
-    print(es_cluster.status())
-    print(us_cluster.health())
+    print(es_cluster.info())
 
 if __name__ == "__main__":
     main()
