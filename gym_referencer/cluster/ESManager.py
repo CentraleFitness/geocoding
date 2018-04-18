430e6d8f7d5eea6fb232a03fd16ef684e5e2e41b
Léo POULIN <leoraphael.poulin@epitech.eu>
2018-04-18 11:55:27 -0700

    getting ready for gym

--- gym_referencer/cluster/ESManager.py
+++ gym_referencer/cluster/ESManager.py
@@ -1,3 +1,5 @@
+from logging import getLogger
+
 from elasticsearch import Elasticsearch
 from config import ES_CLUSTER
 
@@ -11,4 +13,49 @@ class ESManager(object):
             [{'host': ES_CLUSTER['host'], 'port': ES_CLUSTER['port']}],
             timeout=ES_CLUSTER['timeout'],
             max_retries=ES_CLUSTER['max_retries'])
+        self.logger = getLogger(__name__)
         return super().__init__(*args, **kwargs)
+
+    def _create_index(self, name: int, body: dict) -> bool:
+        """
+        Create an index in the cluster
+        """
+        try:
+            ret = self._es.indices.create(index=str(name), body=body)
+        except Exception as ex:
+            self.logger.error(
+                "Exception occured in _create_index: {}".format(ex))
+            return False
+        self.logger.info("Created index {}".format(name))
+        return ret['acknowledged']
+
+    def create_auto_index(self, name: str) -> bool:
+        """
+        Create the _default_ template in the cluster
+        """
+        res = self._create_index(name, body={
+            'settings': {
+                'index': {
+                    'number_of_shards': 5,
+                    'number_of_replicas': 1
+                    }
+                },
+            'mappings': {
+                '_default_': {
+                    'dynamic_templates': [
+                        {
+                            'geo_location': {
+                                'mapping': {
+                                    'type': 'geo_point',
+                                    'fielddata': {
+                                        'format': 'compressed',
+                                        'precision': '3m'
+                                        }
+                                    },
+                                'match': 'location'
+                                }
+                        }
+                        ]
+                    }
+                }
+            })
--- gym_referencer/gym_referencer.py
+++ gym_referencer/gym_referencer.py
@@ -1,10 +1,15 @@
 import json
+import logging.config
 
 from cluster import ESManager
+from config import LOGGING_DICT
+
+logging.config.dictConfig(LOGGING_DICT)
 
 def main() -> None:
     es_cluster = ESManager()
     print(es_cluster._es.info())
+    es_cluster.create_auto_index('gym')
 
 if __name__ == "__main__":
     main()
